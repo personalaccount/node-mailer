c6d43e954936ec8f79e3226dff75254dd81e1371
Philip Ivanov <philip.i@outlook.com>
2019-11-16 02:00:58 +0300

    added PORT env. variable for dynamic binding

--- server/index.js
+++ server/index.js
@@ -3,3 +3,15 @@ const express = require('express');
 
 // Create an express application
 const app = express();
+
+// Routes
+
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+});
+
+// Used for dynamic port binding on from Heroku
+// N.B: environment variable will be injected by Heroku
+const PORT = process.env.PORT;
+
+app.listen(5000);
