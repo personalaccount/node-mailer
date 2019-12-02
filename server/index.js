8a97def73e36159f26ab89a161d92afdd82db1e9
Philip Ivanov <philip.i@outlook.com>
2019-12-02 23:56:23 +0300

    code refactoring - relocated routes to separate files

--- server/index.js
+++ server/index.js
@@ -1,10 +1,5 @@
 // Import necessary libs
 const express = require('express'); // Express app framework
-const passport = require('passport'); // authentication middleware for Node.js
-const passGoogleStrategy = require('passport-google-oauth20').Strategy; // google strategy for passport
-
-// Import clientid and secret keys
-const keys = require('./config/keys');
 
 /*
  Dynamic port binding for Heroku
@@ -14,34 +9,5 @@ const PORT = process.env.PORT || 5000;
 
 // Create an Express application
 const app = express();
-
-passport.use(
-    new passGoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback' // the route that the user will be sent to after they grant permissions to the application
-    }, (accessToken, refreshToken, profile, done) => {
-        console.log('accessToken: ', accessToken);
-        console.log('refreshToken: ', refreshToken);
-        console.log('profile: ', profile);
-    })
-);
-
-// Routes
-
-// Auth route handler Oauth flow is managed by passport
-app.get(
-    '/auth/google',
-    passport.authenticate('google',{
-        scope: ['profile', 'email']
-    })
-);
-
-
-// Handle google oauth redirect
-app.get(
-    '/auth/google/callback',
-    passport.authenticate('google')
-    );
 
 app.listen(PORT);
---
+++ server/routes/auth.js
@@ -0,0 +1,34 @@
+const passport = require('passport'); // authentication middleware for Node.js
+const passGoogleStrategy = require('passport-google-oauth20').Strategy; // google strategy for passport
+
+// Import clientid and secret keys
+const keys = require('../config/keys');
+
+passport.use(
+    new passGoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback' // the route that the user will be sent to after they grant permissions to the application
+    }, (accessToken, refreshToken, profile, done) => {
+        console.log('accessToken: ', accessToken);
+        console.log('refreshToken: ', refreshToken);
+        console.log('profile: ', profile);
+    })
+);
+
+module.exports = app => {
+    // Auth route handler Oauth flow is managed by passport
+    app.get(
+        '/auth/google',
+        passport.authenticate('google', {
+            scope: ['profile', 'email']
+        })
+    );
+
+    // Handle google oauth redirect
+    app.get(
+        '/auth/google/callback',
+        passport.authenticate('google')
+    );
+
+}
