ce212d617458b626bfb4324c0aff81b16b381ec6
Philip Ivanov <philip.i@outlook.com>
2019-12-10 23:48:51 +0300

    user schema

--- server/models/User.js
+++ server/models/User.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 // const { Schema } = mongoose; // The mongoose obj. has a property called Schema, take this property and assign it to the var. Schema
 
-// pass an object to describe all the properties
+// Create a user schema and pass an object to describe all the properties
 const userSchema = new Schema ({
     googleId: String,
     firstName: String,
     lastName: String
 });
 
-// create a new collection for users
+// Load the user schema into Mongoose
 mongoose.model('users', userSchema);
 
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,10 @@
 const passport = require('passport'); // authentication middleware for Node.js
-const keys = require('../config/keys');
 const passGoogleStrategy = require('passport-google-oauth20').Strategy; // google strategy for passport
+const mongoose = require('mongoose'); // require Mongoose library
+const keys = require('../config/keys');
+
+// Fetch the users model from mongoose
+const User = mongoose.model('users'); // User is now a model class - handle to the underlying collection
 
 passport.use(
     new passGoogleStrategy({
@@ -8,9 +12,13 @@ passport.use(
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback' // the route that the user will be sent to after they grant permissions to the application
     }, (accessToken, refreshToken, profile, done) => {
-        console.log('accessToken: ', accessToken, '\n',
-            'refreshToken: ', refreshToken, '\n',
-            'profile: ', profile);
+        // Create a new instance of user
+        new User({
+            googleId: profile.id
+        });
+        // console.log('accessToken: ', accessToken, '\n',
+        //     'refreshToken: ', refreshToken, '\n',
+        //     'profile: ', profile);
     })
 );
 
