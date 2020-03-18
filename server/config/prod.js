78025b7a069c69bfb943353767a8eb113bdbf17b
Philip Ivanov <philip.i@outlook.com>
2020-03-18 18:13:46 +0300

    updated react-scripts and added react-tripe-checkout

--- server/config/prod.js
+++ server/config/prod.js
@@ -3,5 +3,7 @@ module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE_KEY
+    cookieKey: process.env.COOKIE_KEY,
+    // stripeSecretKey: 'sk_test_Oo9dBQqpX1h0NJtXDTN3yxol00VPVDM3a7'
+
 }
