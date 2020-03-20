f0f4de0e4dfe1fe84576e79d38ef2ded6a089364
Philip Ivanov <philip.i@outlook.com>
2020-03-20 23:51:51 +0300

    minor update

--- server/client/src/tmp-views/Dashboard.js
+++ server/client/src/tmp-views/Dashboard.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { fetchUser } from "../actions/index";
-// import SurveyList from './surveys/SurveyList';
 
 const Dashboard = () => {
   return class extends React.Component {
     render() {
       return (
         <div className="container">
-          Hello! {fetchUser}
+          Hello username!
           <div className="fixed-action-btn">
             <Link to="/surveys/new" className="btn-floating btn-large red">
               <i className="material-icons">add</i>
