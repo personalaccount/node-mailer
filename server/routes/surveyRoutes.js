e15cdb9fd5589f347c635a26176258a0f31f1c4d
Philip Ivanov <philip.i@outlook.com>
2020-04-05 22:24:39 +0300

    updated survey post route

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,9 +1,20 @@
+const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin"); // require auth middleware to check if the user is logged in
 const requireCredits = require("../middlewares/requireCredits");
 
+const Survey = mongoose.model("surveys");
+
 module.exports = app => {
   app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
+    const { title, subject, body, recipients } = req.body; // ES6 style destructuring to pull out the required properties
 
-    next();
+    const survey = new Survey({
+      title: title,
+      subject: subject, // it is possible to just right subject - ES6 style since property and variable name are the same
+      body: body,
+      recipients: recipients.split(",").map(email => ({ email: email.trim() })),
+      _user: req.user.id,
+      dateSent: Date.now()
+    });
   });
 };
