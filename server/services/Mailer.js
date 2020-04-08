088ccc9c7773c9ce923e7553454c11b163341198
Philip Ivanov <philip.i@outlook.com>
2020-04-08 22:50:55 +0300

    Mailer constructor

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -2,6 +2,16 @@ const sendgrid = require("sendgrid");
 const helper = sendgrid.mail; //helper obj. that helps create the mailer
 const keys = require("../config/keys.js");
 
-class Mailer extends helper.Mail {}
+class Mailer extends helper.Mail {
+  constructor(survey, content) {
+    const { subject, recipients } = survey;
+    super();
+
+    this.from_email = new.helper.Email("no-reply@recruiforce");
+    this.subject = subject;
+    this.body = new helper.Content("text/html", content);
+    this.recipients = this.formatAddresses(recipients);
+  }
+}
 
 module.exports = Mailer;
